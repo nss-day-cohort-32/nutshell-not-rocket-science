f75e6ab0cbe8dbbf8a49d10754be259223d7183e
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-05-08 10:13:30 -0500

    Committing initial framework to master branch

---
+++ README.md
@@ -0,0 +1,1 @@
+# nutshell-not-rocket-science
---
+++ src/scripts/modules/buildDOMElement.js
@@ -0,0 +1,15 @@
+module.exports.buildDOMElement = function (tagName, parentElement, id, classArray, attributesObject) {
+  let element = document.createElement(tagName);
+  if (id) element.id = id;
+  if (classArray.length > 0) {
+    classArray.forEach(cls => {
+      element.classList.add(cls)
+    });
+  }
+  if (attributesObject) {
+    for (const key of Object.keys(attributesObject)) {
+      element.setAttribute(key, attributesObject[key]);
+    }
+  }
+  return parentElement.appendChild(element);
+}
