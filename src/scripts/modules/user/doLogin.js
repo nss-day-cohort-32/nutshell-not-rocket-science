1d3954c705acf56fd4a722975cbf2c003fd69f66
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-05-10 14:50:14 -0500

    Refactored user login and register methods

---
+++ src/scripts/modules/user/doLogin.js
@@ -0,0 +1,70 @@
+import {
+  getUser,
+  updateOnlineStatus
+} from "../API/users";
+import {
+  setSessionStorage
+} from "../sessionStorage";
+
+
+export function doLogin(userFromRegister) {
+
+  let user;
+  if (userFromRegister) user = userFromRegister;
+  else user = getUserData();
+
+
+
+  try {
+    authenticate(user)
+      .then(user => updateOnlineStatus(user, true))
+      .then(signIn);
+  } catch (e) {
+    handleLoginErrors(e);
+  }
+}
+
+
+
+
+
+
+function authenticate(input) {
+
+  // fetch for matching username/email (same input box)
+  return Promise.all([
+      getUser(`?userName=${input.userName}`),
+      getUser(`?email=${input.email}`)
+    ])
+    .then(
+      authMethods => {
+        console.log(authMethods);
+        let output = authMethods.find(method => {
+          if (method.length === 0) return false;
+          return method[0].password === input.password;
+        });
+        if (output === undefined) throw "Invalid username/password";
+        return output[0];
+      });
+}
+
+
+
+
+
+
+function signIn(user) {
+  if (typeof (user) === "string") console.log(user);
+  else {
+    // Complete login tasks, switch view to main app
+    updateOnlineStatus(user)
+      .then(setSessionStorage);
+  }
+}
+
+
+
+
+function handleLoginErrors(message) {
+  console.log(message);
+}
