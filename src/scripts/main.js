158183cba17eb5fa2e252db9cb7f5fbc84674350
Your Name <you@example.com>
2019-05-10 15:07:38 -0500

    not sure what this was now

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,5 @@
 import API from "./modules/API.js";
-import "./modules/tasks/dbCalls";
-import "./modules/tasks/tasksMain";
-import "./modules/tasks/taskList";
+// import "./modules/tasks/dbCalls";
+// import "./modules/tasks/tasksMain";
+// import "./modules/tasks/taskList";
 import "./modules/users/registration";
