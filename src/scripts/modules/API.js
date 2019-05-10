c5485ebe157498491c2b780826d78c07fe1edc1c
Your Name <you@example.com>
2019-05-10 09:11:48 -0500

    js to create html elements for registration

--- src/scripts/modules/API.js
+++ src/scripts/modules/API.js
@@ -9,12 +9,12 @@ module.exports.API = {
 function Endpoint(url) {
   this.create = (obj) => {
     return fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(obj)
-      })
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(obj)
+    })
       .then(response => response.json());
   };
   this.read = (params) => {
@@ -25,31 +25,31 @@ function Endpoint(url) {
   };
   this.update = (id, object) => {
     return fetch(`${url}/${id}`, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(object)
-      })
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(object)
+    })
       .then(response => response.json());
   };
   this.replace = (id, newObject) => {
     return fetch(`${url}/${id}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(newObject)
-      })
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newObject)
+    })
       .then(response => response.json());
   };
   this.delete = (id) => {
     return fetch(`${url}/${id}`, {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json"
-        }
-      })
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
       .then(response => response.json());
   };
 
--- src/scripts/modules/users/registration.js
+++ src/scripts/modules/users/registration.js
@@ -1,1 +1,34 @@
-// Collect the data from the input fields and build an object when the button is pressed, then send that object to the db with a fetch POST
+import api from "../API.js";
+
+
+const mainDiv = document.querySelector("#main-content-area");
+
+//login function
+
+//this needs to be a fetch to the DB to check if the user exists. This should be an API call. If the user exists set a login token with sessionStorage.setItem(), if the user doesn't exist, make a second api call to "register" the user and write their info to the database
+
+
+
+//Create all HTML elements
+
+let loginDiv = document.createElement("div");
+let inputForm = document.createElement("form");
+let userNameInput = document.createElement("input");
+let emailInput = document.createElement("input");
+let passwordInput = document.createElement("input");
+let signInBtn = document.createElement("button");
+// let registerBtn = document.createElement("button");
+
+emailForm.setAttribute("type", "email");
+passwordForm.setAttribute("type", "password");
+
+let formArray = [userNameInupt, emailInput, passwordInput, signInBtn];
+formArray.forEach(element => {
+    inputForm.appendChild(element);
+});
+
+loginDiv.appendChild(inputForm);
+mainDiv.appendChild(loginDiv);
+
+inputForm.addEventListener("submit", logUserIn);
+
