05fd2f834fdbd17f9475305208785288936060d4
Your Name <you@example.com>
2019-05-13 09:14:36 -0500

    worked on articles code

--- src/scripts/modules/users/Articles.js
+++ src/scripts/modules/users/Articles.js
@@ -1,7 +1,7 @@
 import api from "../API";
 
 // target main div
-const mainDiv = document.querySelector("#main-content-area")
+const mainDiv = document.querySelector("#main-content-area");
 
 // base code framework -- needs work
 api.getAllArticles()
@@ -10,26 +10,27 @@ api.getAllArticles()
         articlesArray.forEach(article => {
 
             //Create elements
-            let articleDiv = document.createElement("div")
-            let articleTitle = document.createElement("h2")
-            let articleUrl = document.createElement("h4")
-            let articleSyn = document.createElement("p")
+            let articleDiv = document.createElement("div");
+            let articleTitle = document.createElement("h2");
+            let articleUrl = document.createElement("h4");
+            let articleSyn = document.createElement("p");
 
             //Set elements value to values in current object in loop
-            articleTitle.innerHTML = article.articleTitle
-            articleUrl.innerHTML = article.url
-            articleSyn.innerHTML = article.synopsis
+            articleTitle.innerHTML = article.articleTitle;
+            articleUrl.innerHTML = article.url;
+            articleSyn.innerHTML = article.synopsis;
 
             //append to div
-            articleDiv.appendChild(articleTitle)
-            articleDiv.appendChild(articleUrl)
-            articleDiv.appendChild(articleSyn)
+            articleDiv.appendChild(articleTitle);
+            articleDiv.appendChild(articleUrl);
+            articleDiv.appendChild(articleSyn);
 
             //append to list div
-            articleDivList.appendChild(articleDiv)
+            articleDivList.appendChild(articleDiv);
 
-        })
+        });
         //Append all articles to main div
         mainDiv.appendChild(articleDivList);
 
+        // eslint-disable-next-line semi
     })
