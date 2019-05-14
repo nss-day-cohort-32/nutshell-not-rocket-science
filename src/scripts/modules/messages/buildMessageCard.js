f27eb6f6ea0c34b6a1da7f270eef5c17f2d6fb05
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-05-14 11:47:04 -0500

    Added button for other users, started work on the "Add friend" button.

--- src/scripts/modules/messages/buildMessageCard.js
+++ src/scripts/modules/messages/buildMessageCard.js
@@ -8,12 +8,13 @@ import {
 export function buildMessageCard(message) {
   let article = document.createElement("article"),
     authorTools = "",
+    addFriend = "",
     loggedInUser = getLoggedInUser();
   article.setAttribute("data-timeStamp", message.postedTime);
   article.classList.add("message");
   article.id = `message--${message.id}`;
-  console.log(message.userId);
-  console.log(loggedInUser.id);
+
+
   if (message.userId === loggedInUser.id) {
     authorTools = `
     <div class="message--icon-container">
@@ -24,6 +25,8 @@ export function buildMessageCard(message) {
     </div>
   `;
     article.classList.add("message-fromMe");
+  } else {
+    addFriend = `<div class="message-plus-icon" id="message-add-friend-${message.userId}"><ion-icon name="add-circle-outline"></ion-icon></div>`;
   }
   let messageTime = new Date(message.postedTime * 1000).toLocaleString(
     "en-US", {
@@ -40,7 +43,7 @@ export function buildMessageCard(message) {
     </div>
     <div class="message-tools">
       <aside class="message-author">
-        ${message.user.userName}
+        ${message.user.userName} ${addFriend}
       </aside>
       <aside class="message-time">
         ${messageTime}
--- src/scripts/modules/populateFriendList.js
+++ src/scripts/modules/populateFriendList.js
@@ -1,25 +1,56 @@
-import { getLoggedInUser } from "./helpers/sessionStorage";
+import {
+    getLoggedInUser
+} from "./helpers/sessionStorage";
 
 export const getFriendList = () => {
     let userObj = getLoggedInUser();
 
     fetch(`http://localhost:8088/friends?sourceId=${userObj.id}&_expand=user`)
-    .then(response => response.json())
-    .then(data => {
-    console.log(data);
-    console.log("more", data[0].id);
-    const friendDiv = document.createElement("div");
-    friendDiv.id = `friend-${data[0].id}`;
+        .then(response => response.json())
+        .then(data => {
+            let friendListHeader = document.querySelector(".sidebar-friendList-header");
+            friendListHeader.innerHTML = "";
+            console.log(data);
+            console.log("more", data[0].id);
+            const friendDiv = document.createElement("div");
+            friendDiv.id = `friend-${data[0].id}`;
 
-    data.forEach(friend => {
-        console.log(friend);
-        const html = `
+            data.forEach(friend => {
+                console.log(friend);
+                const html = `
         <h3 id="friend-${friend.id}">${friend.user.userName}</h3>
         <button id="deleteFriend-${friend.id}>Delete Friend</button>
         `;
-    friendDiv.innerHTML += html;
-    });
+                friendDiv.innerHTML += html;
+            });
 
-    document.querySelector(".sidebar-friendList-header").appendChild(friendDiv);
-    });
+            friendListHeader.appendChild(friendDiv);
+        });
 };
+
+
+export const addFriend = function (person1Id, person2Id) {
+    fetch("http://localhost:8088/friends", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify([{
+                    sourceId: person1Id,
+                    userId: person2Id
+                },
+                {
+                    sourceId: person2Id,
+                    userId: person1Id
+                }
+            ])
+        })
+        .then(response => response.json())
+        .then(getFriendList);
+};
+
+// {
+//     "id": 1,
+//     "sourceId": 1,
+//     "userId": 2
+//   },
